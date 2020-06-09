f3ceeabbfe65b461b411949c355ab94009cc8acf
sarthak <sarthakjaiswal45@gmail.com>
2020-06-09 12:33:49 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express=require("express");
+
+
+
+const app=express();
+app.get("/",function(req,res){
+  res.send("api running");
+});
+
+
+
+
+
+
+
+
+
+const PORT=process.env.PORT||5000;
+app.listen(PORT,()=>console.log(`server started on port ${PORT}`));
